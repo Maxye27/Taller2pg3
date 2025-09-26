d6f27096f8b205885798eb168a42f84aa9341a2e
Max <mjimenez21@udi.edu.co>
2025-09-25 19:04:17 -0500

    primer version

---
+++ Persona.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Taller2pg
+{
+    public class persona
+    {
+
+        public string Nombre{ get; set;}
+        public float Peso { get; set;} 
+        public float Altura { get; set;}
+        public int Edad { get; set;}
+        public string Genero { get; set;}
+        public string Ocupacion { get; set;}
+        public string Ciudad { get; set;}
+        public string Estadocivil { get; set;}
+
+        public persona()
+        {
+            Nombre = "";
+            Peso = 0f;
+            Altura = 0f;
+            Edad = 0;
+            Genero = "";
+            Ocupacion = "";
+            Ciudad = "";
+            Estadocivil = "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Taller2pg;
+    internal class Program
+    {
+    private static void Main(string[] args)
+    {
+        persona perso = new persona();
+        Console.WriteLine("Digite su nombre: ");
+        perso.Nombre = Console.ReadLine();
+        Console.WriteLine("Digite su peso: ");
+        perso.Peso = float.Parse(Console.ReadLine());
+        Console.WriteLine("Digite su altura: ");
+        perso.Altura = float.Parse(Console.ReadLine());
+        Console.WriteLine("Digite su edad: ");
+        perso.Edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite su genero: ");
+        perso.Genero = Console.ReadLine();
+        Console.WriteLine("Digite su ocupacion: ");
+        perso.Ocupacion = Console.ReadLine();
+        Console.WriteLine("Digite su ciudad: ");
+        perso.Ciudad = Console.ReadLine();
+        Console.WriteLine("Digite su estado civil: ");
+        perso.Estadocivil = Console.ReadLine();
+        
+        Console.WriteLine("Su nombre es: " + perso.Nombre);
+        Console.WriteLine("Su peso es: " + perso.Peso);
+        Console.WriteLine("Su altura es: " + perso.Altura);
+        Console.WriteLine("Su edad es: " + perso.Edad);
+        Console.WriteLine("Su genero es: " + perso.Genero);
+        Console.WriteLine("Su ocupacion es: " + perso.Ocupacion);
+        Console.WriteLine("Su ciudad es: " + perso.Ciudad);
+        Console.WriteLine("Su estado civil es: " + perso.Estadocivil);
+
+        }
+    }
